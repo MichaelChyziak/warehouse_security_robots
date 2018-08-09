07f3d434b735e422713ddaa792e92df61d753a46
TheChyz <mchyziak@sfu.ca>
2018-08-09 11:46:13 -0700

    Untested chasing algorithm implemented, waiting for found function

--- intruder/intruder.h
+++ intruder/intruder.h
@@ -3,10 +3,12 @@
 
 #include "../graph/graph.h"
 #include "../warehouse/warehouse.h"
+#include "../robots/robots.h"
 
 std::vector<std::vector<unsigned int>> intruderStart(unsigned int start_intruder_position, unsigned int current_index);
 bool intruderValidMove(unsigned int move);
 std::vector<std::vector<unsigned int>> intruderMovePatrol(unsigned int move, unsigned int current_index);
+std::vector<std::vector<unsigned int>> intruderMoveChased(unsigned int move, unsigned int index);
 bool intruderFound();
 
 #endif // INTRUDER_H

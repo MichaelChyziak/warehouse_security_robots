d136957ed100fd9aad3be877d90d2123eb529e28
mchyziak <mchyziak@asb9840u-a03.csil.sfu.ca>
2018-08-08 22:16:24 -0700

    Warehouse patrol function

--- graphGen/graphGen.h
+++ graphGen/graphGen.h
@@ -10,7 +10,8 @@
 const unsigned int PATH     = 0;
 const unsigned int WALL     = 1;
 const unsigned int DOOR     = 2;
-const unsigned int OBSTACLE = 3;
+const unsigned int ROBOT    = 3;
+const unsigned int INTRUDER = 4;
 
 std::vector< std::vector<unsigned int>> createWarehouseGraph(std::vector< std::vector<unsigned int> > warehouse);
 std::vector< std::vector<unsigned int>> createWarehouseGraphMichael(std::vector< std::vector<unsigned int> > warehouse);
---
+++ warehouse/warehouse.h
@@ -0,0 +1,10 @@
+#ifndef WAREHOUSE_H
+#define WAREHOUSE_H
+
+#include "../graph/graph.h"
+#include "../graphGen/graphGen.h"
+#include "../chinese_postman/chinese_postman.h"
+
+std::vector<std::vector<unsigned int>> warehousePatrolUpdate(unsigned int current_index);
+
+#endif
